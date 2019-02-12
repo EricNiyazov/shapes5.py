a48f1929585a911e26c53397cd9f5c45aa833c0d
csPython <cspython@AD25TF04NGF1J-10.local>
2019-02-12 11:47:51 -0500

    added the orange rombas in th corner

--- shapes5.py
+++ shapes5.py
@@ -6,10 +6,10 @@ bTri.setFill(color_rgb(30, 30,230))
 gSqur = Polygon(Point(500, 500), Point(500, 600), Point (600, 600), Point(600, 500))
 gSqur.setFill(color_rgb(30, 230, 30))
 
-##orRomb = Polygon(Point(500, 500), Point(500, 600), Point (600, 600), Point(600, 500))
-##orSqur.setFill(color_rgb(30, 230, 30))
-##orSqur.draw(window)
+orRomb = Polygon(Point(800, 100), Point(850, 200), Point (900, 100), Point(850, 0))
+orRomb.setFill(color_rgb(255,165,0))
 
+orRomb.draw(window)
 gSqur.draw(window)
 bTri.draw(window)
 
