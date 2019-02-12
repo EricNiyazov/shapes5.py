d08ab6ca1817492a50915745bdb4bb1c5d280d99
csPython <cspython@AD25TF04NGF1J-10.local>
2019-02-12 11:41:39 -0500

    add the window it is now working

--- shapes5.py
+++ shapes5.py
@@ -1,1 +1,19 @@
 from graphics import *
+window = GraphWin("shapes5.py", 300, 300)
+window.setCoords(0, 0, 1000, 1000)
+bTri = Polygon(Point(100, 100), Point(150, 200), Point (200, 100))
+bTri.setFill(color_rgb(30, 30,230))
+gSqur = Polygon(Point(500, 500), Point(500, 600), Point (600, 600), Point(600, 500))
+gSqur.setFill(color_rgb(30, 230, 30))
+
+##orromb = Polygon(Point(500, 500), Point(500, 600), Point (600, 600), Point(600, 500))
+##gSqur.setFill(color_rgb(30, 230, 30))
+##gSqur.draw(window)
+
+gSqur.draw(window)
+bTri.draw(window)
+
+
+
+window.getMouse()
+window.close()
